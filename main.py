fee316fcdd8e29eb5e72fb9bb002220f12a28a79
WishmasterAku <WishmasterAkunin@gmail.com>
2023-03-22 11:33:14 -0500

    Added Speed and Completed Solar System Project

--- main.py
+++ main.py
@@ -1,48 +1,6 @@
 from turtle import *
 
-#Challenge: Create a Solar Like System with Different Color and Size Planets
-bgcolor("black")
 
-color("orange")
-begin_fill()
-circle(30)
-end_fill()
-
-penup()
-forward(80)
-pendown()
-
-color("red")
-begin_fill()
-circle(20)
-end_fill()
-
-penup()
-forward(80)
-pendown()
-
-color("green")
-begin_fill()
-circle(40)
-end_fill()
-
-penup()
-forward(120)
-pendown()
-
-color("blue")
-begin_fill()
-circle(60)
-end_fill()
-
-penup()
-forward(140)
-pendown()
-
-color("yellow")
-begin_fill()
-circle(50)
-end_fill()
 
 done()
 

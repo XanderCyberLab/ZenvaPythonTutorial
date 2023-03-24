a823958aa916eeb738633745c0097f94b274f55d
WishmasterAku <WishmasterAkunin@gmail.com>
2023-03-24 11:50:27 -0500

    Bit more conditions

--- Conditions.py
+++ Conditions.py
@@ -13,3 +13,34 @@ if  a < b:
 if a > b:
     print("a is greater than b")
     
+name = "Bob"
+if name == "Jack":
+    print("You are Jack")
+elif name == "Bob":
+    print("You are Bob")
+else:
+    print("You are not Jack or Bob")
+    
+a = 5
+b = 5
+
+if a == b:
+  print("a is equal to b")
+
+if a > b:
+  print("a is greater than b")
+
+if a < b:
+  print("a is less than b")
+  
+if a != b:
+  print("a doesn't equal b")
+  
+if a >= b:
+  print("a is greater than or equals to b")
+
+if a <= b:
+  print("a is less than or equals to b")
+  
+
+    

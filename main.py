3884c0addf293e98a7998af8c04dc6c3f216f91f
WishmasterAku <WishmasterAkunin@gmail.com>
2023-03-30 00:10:43 -0500

    Started Balloon Pop Project

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from turtle import *
 
-speed (5)
 bgcolor("black")
-
-def move_and_turn (distance, angle):
-    forward(distance)
-    right(angle)
-    
 color("red")
-move_and_turn(100, 45)
-move_and_turn(50, 90)
-move_and_turn(100, 45)
+
+begin_fill()
+circle(50)
+end_fill()
+
 done()
-    
 
 
 

079a4709f058c03674c39d24761b6142ce99ae55
Wishymashy <amoralesmm@yahoo.com>
2023-03-31 16:56:21 -0500

    Added Onkey Movement for Turtle

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from turtle import *
 
 setup(600, 400)
 bgcolor("#C2B280")
-move_distance = 20
+move_distance = 100
 
 penup()
 color("blue")
@@ -24,5 +24,26 @@ pendown()
 shape("turtle")
 color("green")
 
+def move_up ():
+    setheading(90)
+    forward(move_distance)
+    
+def move_down ():
+    setheading(270)
+    forward(move_distance)
+
+def move_left ():
+    setheading(180)
+    forward(move_distance)
+
+def move_right ():
+    setheading(0)
+    forward(move_distance)
+    
+onkey(move_up, "Up")
+onkey(move_down, "Down")
+onkey(move_left, "Left")
+onkey(move_right, "Right")
+listen()
 
 done()

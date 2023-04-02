502864da5987d81825de2a82e2e9b1c8204a0789
WishmasterAku <xandertoolz@gmail.com>
2023-04-01 20:03:26 -0500

    Welcome Prompt

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 print("Medical Diagnose Bot")
 
-name = input("What is your name? ")
-print("Welcome " + name + "! What would you like to do today?")
 
 
+welcome_prompt = "Welcome Doctor! What would you like to do today?\n - To list all patients, press 1\n - To Run a new diagnosis, press 2\n - To quit, press q\n"
 
 
+def main():
+    selection = (welcome_prompt)
+    print(selection)
 

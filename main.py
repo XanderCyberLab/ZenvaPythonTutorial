3b230072037810a5900c33a6994b3c40ded87b06
WishmasterAku <xandertoolz@gmail.com>
2023-04-01 20:58:51 -0500

    Created a Menu for selections

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 print("Medical Diagnose Bot")
+welcome_prompt = "Welcome Doctor! Please choose an option from the menu below\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
 
+def list_patients():
+    print("Listing patients")
 
+def run_diagnosis():
+    print("New Diagnosis")
 
-welcome_prompt = "Welcome Doctor! What would you like to do today?\n - To list all patients, press 1\n - To Run a new diagnosis, press 2\n - To quit, press q\n"
+def main():     
+    
+    selection = input(welcome_prompt)
+    if selection == "1":
+        list_patients()
+    elif selection == "2":
+        run_diagnosis()
+    elif selection == "q":
+        return
 
-
-def main():
-    selection = (welcome_prompt)
-    print(selection)
-
+main()

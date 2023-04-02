0b89cad9ba20b76855cb7bb77eba904278ecb8e1
WishmasterAku <xandertoolz@gmail.com>
2023-04-01 22:47:27 -0500

    Created a diagnosis option to enter patients name

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 print("Medical Diagnose Bot")
-welcome_prompt = "Welcome Doctor! Please choose an option from the menu below\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
+welcome_prompt = "Welcome " + doctor + "Doctor! Please choose an option from the menu below\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
+name_prompt = "What is the patient's name?\n"
+doctors_name ="Enter your name"
 
 def list_patients():
     print("Listing patients")
 
 def run_diagnosis():
-    print("New Diagnosis")
+    name = input(name_prompt)
 
 def main():     
-    
-    selection = input(welcome_prompt)
-    if selection == "1":
-        list_patients()
-    elif selection == "2":
-        run_diagnosis()
-    elif selection == "q":
-        return
+    doctor = input(name_prompt)
+    while(True):
+        selection = input(welcome_prompt)
+        if selection == "1":
+            list_patients()
+        elif selection == "2":
+            run_diagnosis()
+        elif selection == "q":
+            return
 
 main()

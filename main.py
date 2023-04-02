38aa522d2503ef5b65672ab342b5b9a4da0e7100
WishmasterAku <xandertoolz@gmail.com>
2023-04-01 22:50:23 -0500

    Minor fixes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 print("Medical Diagnose Bot")
-welcome_prompt = "Welcome " + doctor + "Doctor! Please choose an option from the menu below\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
+welcome_prompt = "Welcome Doctor! Please choose an option from the menu below\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
 name_prompt = "What is the patient's name?\n"
-doctors_name ="Enter your name"
+apperance_prompt = "How's the patient general appearance?\n - 1: Normal apperance\n - 2: Irritable or lethargic\n"
 
 def list_patients():
     print("Listing patients")
 
 def run_diagnosis():
     name = input(name_prompt)
+    apperance = input(apperance_prompt)
 
 def main():     
-    doctor = input(name_prompt)
     while(True):
         selection = input(welcome_prompt)
         if selection == "1":

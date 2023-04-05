db3c6e7d25c5c6576871b4140eb15db757d229fd
Wishymashy <amoralesmm@yahoo.com>
2023-04-05 13:57:42 -0500

    Adjusted the List to include a Dictionary for each user. List Task by Users

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ welcome_prompt = "Welcome to Wishymashy's Notepad!\n Please Select an Option:\n 
 new_user_prompt = "Please enter a new username:\n"
 tasks_prompt ="Please select a task:\n 1 - List Saved Tasks\n 2 - Create a Task\n"
 
-users_database = [
-    'Alexander',
-    'Miriam',
-    'Juvia' 
-]
+users_database = {
+    'Alexander': [],
+    'Miriam': [],
+}
 
 users_task = [
     "Test1: Category: Task 1",
@@ -15,24 +14,26 @@ users_task = [
 
 
 def list_users():
-    for users in users_database:
-        print(users)
+    for user in users_database:
+        print(user)
 
 def list_tasks(selected_user):
-    for tasks in users_task:
-        print(tasks)
+    print(f'List of Tasks for {selected_user}:\n')
+    tasks = users_database[selected_user]
+    for task in tasks:
+        print(task)
 
 def new_user():
     username = input(new_user_prompt)
-    users_database.append(username)
-    print("New User Created: " + username + "\n")
+    users_database[username] = []
+    print(f'New User Created: {username}\n')
     
 def new_task(selected_user):
     print(f"Please enter a new task for {selected_user}:\n")
-    task_name = input()
-    task = f"{selected_user}: {task_name}"
-    users_task.append(task)
-    print(f"New task '{task}' created for {selected_user}\n")    
+    task = input()
+    tasks = users_database[selected_user]
+    users_database[selected_user].append(task)
+    print(f'New Task Created: {task} for user {selected_user}\n') 
 
 def task_user(selected_user):#Passes the selected user from select_user function
     task_selection = input(tasks_prompt)
@@ -48,8 +49,9 @@ def select_user():
     for index, username in enumerate(users_database):#For loop with enumerate is to # the list
         print(f'{index + 1}. {username}')
     user_selection = int(input()) - 1 #-1 is to make the list start at 0
-    selected_user = users_database[user_selection] #This is to select the user from the list  
+    selected_user = list(users_database.keys())[user_selection] #This is to select the user from the list  
     print(f'Welcome {selected_user}\n')
+    task_user(selected_user)
     return selected_user #This is to return the selected user to the main function
 
 def main():
@@ -61,7 +63,7 @@ def main():
             new_user()
         elif selection == "3":
             selected_user = select_user()#Creates a variable and returns the selected user from select user function
-            task_user(selected_user)#With selected user variable, it is passed to task_user function
+            #task_user(selected_user)#With selected user variable, it is passed to task_user function
         else:
             return ""
             
